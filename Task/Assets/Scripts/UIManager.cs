731f314e3b0985d1b954e14a396888db5053c2c6
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-06-12 22:16:22 +0200

    Update UIManager.cs

--- Task/Assets/Scripts/UIManager.cs
+++ Task/Assets/Scripts/UIManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("To Fill")]
     [SerializeField] private TextMeshProUGUI name;
     [SerializeField] private TextMeshProUGUI hp;
 
@@ -24,29 +25,37 @@ public class UIManager : MonoBehaviour
     public void StartCount()
     {
         InvokeRepeating("Count",1,1);
-        
     }
 
     private void Count()
     {
-       Debug.Log(_timer);
-       if (_timer % 3==0 && _timer % 5==0)
-       {
-           Debug.Log("MarkoPolo");    
-       }
-       else
-       {
-           if (_timer % 3==0)
-           {
-               Debug.Log("Marko");   
-           }
-           if (_timer % 5==0)
-           {
-               Debug.Log("Polo");   
-           }
-       }
-       _timer += 1;
+        Debug.Log(_timer);
+        if (_timer % 3 == 0 && _timer % 5 == 0)
+        {
+            Debug.Log("MarkoPolo");
+        }
+        else
+        {
+            if (_timer % 3 == 0)
+            {
+                Debug.Log("Marko");
+            }
+
+            if (_timer % 5 == 0)
+            {
+                Debug.Log("Polo");
+            }
+
+        }
+
+        if (_timer == 100)
+        {
+            CancelInvoke();
+        }
+
+        _timer += 1;
     }
+
     public void UpdateText(AgentMovement agent)
     {
         name.text = agent.name;
